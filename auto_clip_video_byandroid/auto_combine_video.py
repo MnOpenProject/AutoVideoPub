5fa41f4e137975411242248a7febdf245be9a866
Microl <414464322@qq.com>
2022-02-12 11:25:46 +0800

    fix(auto_clip): update combine function script

--- auto_clip_video_byandroid/auto_combine_video.py
+++ auto_clip_video_byandroid/auto_combine_video.py
@@ -202,6 +202,15 @@ def clear_fullfiles_folder(p_second_dir_videodir):
 def combine_ts_group_by_timeval(paragraph_time_list,ts_file_root_dir,ts_file_video_dir,p_second_dir_videodir,ts_folder_name):
     log_print('当前视频文件的根目录: \n{0}'.format(ts_file_root_dir))
     log_print('具体的 .ts 文件目录：\n{0}'.format(ts_file_video_dir))
+    # 先清空 video/xxx/xxx/fullfiles 目录下已存在的所有文件夹及其视频文件
+    fullfiles_dirlist = os.listdir(p_second_dir_videodir)
+    for dir_name in fullfiles_dirlist:
+        dir_path = f'{p_second_dir_videodir}/{dir_name}'
+        # 删除该目录下的视频文件
+        del_files(dir_path)
+        # 这个操作只会删除一个文件夹（若使用 os.removedirs 会递归删除目录，即若父目录为空文件夹会一直递归删除直到一个父目录下存在其他子文件夹才会停止
+        os.rmdir(dir_path)
+    
     # 获取文件夹下的所有文件名
     ts_file_list = os.listdir(ts_file_video_dir)
     ts_file_list = bubbleSortTsFile(ts_file_list) # 一定要进行按序号名称从小到大排好序，才能确保后续操作
@@ -230,8 +239,8 @@ def combine_ts_group_by_timeval(paragraph_time_list,ts_file_root_dir,ts_file_vid
             ts_file_path = f'{ts_file_video_dir}/{ts_file_name}'
             # 获取当前.ts文件的时长
             duration_s = get_duration_from_ffmpeg(ts_file_path)
-            # 取整
-            duration_s = int(round(float(duration_s),1))
+            # 保留2位小数
+            duration_s = round(float(duration_s),2)
             duration_total_s += duration_s
             # 若累加时长在 开始 和 结束 的范围内，则记录下这个范围内的 .ts 文件的绝对路径
             if duration_total_s >= start_total_sec and duration_total_s <= end_total_sec:
